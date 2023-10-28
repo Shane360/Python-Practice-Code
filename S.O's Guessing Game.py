6e4759767f1869e58aa1921e6f6cb088443bdaa0
Shane360 <52971594+Shane360@users.noreply.github.com>
2023-10-28 01:39:41 +0100

    Add files via upload
    Guessing Game which uses a for loop to ensure the choice of user is limited to 3 before the answer is revealed.
    Random integers are generated each time the game is refreshed and the user is promoted to guess the correct answer.

---
+++ S.O's Guessing Game.py
@@ -0,0 +1,21 @@
+import random
+print('Hello, friend. What is your name?')
+name = input()
+secretNumber = random.randint(0, 10)
+print('Welcome ' + name + '. Lets play a game. I am thinking of a number between 1 and 10. Guess which it is.')
+
+print('The secret number is ' + str(secretNumber))
+for guessesTaken in range(1, 6):
+    print('Take a guess.')
+    guess = int(input())
+    if guess < secretNumber:
+            print('Your guess is too low.')
+    elif guess > secretNumber:
+            print('Your guess is too high')
+    else:
+            break
+
+if guess == secretNumber:
+    print('Good job, ' + name + '! You guessed ' + str(guessesTaken) + ' times!')
+else:
+        print('Nope. The number I was thinking of was ' + str(secretNumber))
