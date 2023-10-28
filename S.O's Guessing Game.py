96b028eb634953c5dd7e58ee4efe3cb7b476ec0d
Shane360 <52971594+Shane360@users.noreply.github.com>
2023-10-28 01:40:42 +0100

    Update S.O's Guessing Game.py

--- S.O's Guessing Game.py
+++ S.O's Guessing Game.py
@@ -4,7 +4,7 @@ name = input()
 secretNumber = random.randint(0, 10)
 print('Welcome ' + name + '. Lets play a game. I am thinking of a number between 1 and 10. Guess which it is.')
 
-print('The secret number is ' + str(secretNumber))
+print('The secret number is ' + str(secretNumber))     #Print the answer before user guesses
 for guessesTaken in range(1, 6):
     print('Take a guess.')
     guess = int(input())
